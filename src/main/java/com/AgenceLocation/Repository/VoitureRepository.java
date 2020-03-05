56c22a1e5eef0bdc265f641394672d8088f5fea0
Mariem-Azl <60945184+Mariem-Azl@users.noreply.github.com>
2020-03-05 11:40:03 +0100

    test

--- src/main/java/com/AgenceLocation/Repository/VoitureRepository.java
+++ src/main/java/com/AgenceLocation/Repository/VoitureRepository.java
@@ -9,8 +9,6 @@ package com.AgenceLocation.Repository;
 import com.AgenceLocation.bean.Voiture;
 //import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
