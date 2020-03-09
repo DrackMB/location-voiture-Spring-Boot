3600a44fb3b71f233bdf807592e68cb566eafc8a
asma <aaoubala99@hotmail.com>
2020-03-09 21:21:16 +0100

    verification

--- src/main/java/com/AgenceLocation/Service/impl/ReviewServiceImpl.java
+++ src/main/java/com/AgenceLocation/Service/impl/ReviewServiceImpl.java
@@ -37,7 +37,7 @@ public class ReviewServiceImpl implements ReviewService {
         return reviewRepository.findAll();
     }
 
-    @Override
+    
     public int save(Review review) {
         Note foundedNote=noteService.FindByLibelle(review.getNote().getLibelle());
         Client foundedClient=clientService.findByCin(review.getClient().getCin());
@@ -61,58 +61,6 @@ public class ReviewServiceImpl implements ReviewService {
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.AgenceLocation.Service.impl;
 
 
-import com.AgenceLocation.Repository.ReviewRepository;
-import com.AgenceLocation.Service.facad.ClientService;
-import com.AgenceLocation.Service.facad.NoteService;
-import com.AgenceLocation.Service.facad.ReviewService;
-import com.AgenceLocation.bean.Client;
-import com.AgenceLocation.bean.Note;
-import com.AgenceLocation.bean.Review;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-/**
- *
- * @author aaoub
- */
-@Service
-public class ReviewServiceImpl implements ReviewService {
-    @Autowired
-    ClientService clientService;
-    @Autowired
-    NoteService noteService ;
-   // @Autowired
-    //VoitureService voiturreService;
-    
-   @Autowired
-   ReviewRepository reviewRepository;
-    @Override
-    public List<Review> findAll() {
-        return reviewRepository.findAll();
-                }
-
-    @Override
-    public int save(Review review) {
-        Client foundedClient=clientService.findByCin(review.getClient().getCin());
-        Note foundedNote=noteService.FindByLibelle(review.getNote().getLibelle());
-      //  Voiture foundedVoiture=voitureRepository.findByMtricule(review.getVoiture().getMatricule());
-        if(foundedClient==null){
-            return -1;
-        }else if(foundedNote==null){
-            return -2;
-        }//else if(foundedVoiture==null){return -3;}  
-        else{
-            reviewRepository.save(review);
-            return 1;
-        }
-        
-        
-
-    }
-    
-    
-}
